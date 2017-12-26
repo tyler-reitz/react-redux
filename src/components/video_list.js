372e0b447bbb8bbc37cb777ef352ccf9cd754f5e
Tyler Reitz <tdreitz@gmail.com>
2017-12-25 16:27:24 -0800

    Replace raw markup w/ VideoList component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,10 @@
 import React from 'react'
+import VideoListItem from './video_list_item'
 
 const VideoList = ({videos}) => (
   <ul className="col-md-4 list-group">
     {videos.map(video => (
-      <li key={video.etag} >
-        {video.snippet.title}
-      </li>
+      <VideoListItem key={video.etag} video={video} />
     ))} 
   </ul>
 )
